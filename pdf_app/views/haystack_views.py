c62a0952d688475c9c1f8ebf325d58f9f00ff055
Shahin <shahin.zeynalov95@gmail.com>
2020-10-17 21:11:02 +0400

    Highlight added

--- pdf_app/views/haystack_views.py
+++ pdf_app/views/haystack_views.py
@@ -3,8 +3,10 @@ from drf_haystack.viewsets import HaystackViewSet
 from ..models import Document
 from ..search_indexes import DocumentIndex
 from rest_framework.permissions import AllowAny, IsAuthenticated
+from drf_haystack.filters import HaystackHighlightFilter
 
 class DocumentSearchView(HaystackViewSet):
     index_models = [Document]
     serializer_class = DocumentHaystackSerializer
     permission_classes = [IsAuthenticated]
+    filter_backends = [HaystackHighlightFilter]
